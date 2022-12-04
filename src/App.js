d79dd481606b68e76ef69674f678b720a8122ee8
rusmarch <jakal5577>
2022-12-04 10:04:44 +0200

    6

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ function App() {
     const [brakeTime, setBrakeTime] = useState(5);
     const [pomodoroTime, setPomodoroTime] = useState(10);
     const [displayTime, setDisplayTime] = useState(pomodoroTime);
-    const [mode, setMode] = useState('work'); // work/brake/null
+    const [mode, setMode] = useState(true);
     const [isPaused, setIsPaused] = useState(true);
 
+// =============== Вынести в компонет Format Time ====================================
     const formatTime = (time) => {
         let minutes = Math.floor(time / 60);
         let seconds = time % 60;
@@ -20,71 +21,65 @@ function App() {
         );
     }
 
+// ===================--  --=======================================================
 
-// ===================-- Кнопки Start и Stop --========================================================
-
-    function tick() {
-        setDisplayTime(displayTime - 1);
-    }
 
     let interval;
-
-    useEffect(() => {
-
-        function switchMode() {
-            const nextMode = mode === 'work' ? 'break' : 'work';
-            setMode(nextMode);
-
-        }
-
-             interval = setInterval(() => {
-            if (isPaused) {
-                return;
-            }
-            if (displayTime === 0) {
-                return switchMode();
-                if (mode === 'work') {
-                    setDisplayTime(pomodoroTime);
-                } else {
-                    setDisplayTime(brakeTime);
-                }
-            }
-            tick();
-        }, 1000)
-
-        return () => clearInterval(interval);
-    })
 
     function stop() {
         clearInterval(interval);
     }
 
+    useEffect(() => {
+        if (isPaused) {
+            return;
+        } else {
+            interval = setInterval(() => {
+                setDisplayTime(displayTime - 1);
+            },1000)
+        }
+
+        if (displayTime === 0) {
+            stop();
+            setIsPaused(true);
+            setMode(!mode);
+        }
+        return () => clearInterval(interval);
+    }, [displayTime, isPaused]);
+
+
+    console.log('mode: ' + mode);
+    console.log('isPaused: ' + isPaused);
+
+
+
+
 // =============================================================================
 
 
-    const changeTime = (amount, type) => {
-        if (type === 'pomodoro') {
-            setPomodoroTime((prev) => prev + amount);
-        } else {
-            setBrakeTime((prev) => prev + amount);
-        }
+const changeTime = (amount, type) => {
+    if (type === 'pomodoro') {
+        setPomodoroTime((prev) => prev + amount);
+    } else {
+        setBrakeTime((prev) => prev + amount);
     }
+}
 
-     console.log('mode: ' + mode);
-    //console.log(displayTime);
+//console.log('mode: ' + mode);
+//console.log(displayTime);
 
-    return (
-        <div className="App">
-            <Length title="Pomodoro length" time={pomodoroTime} changeTime={changeTime} type='pomodoro'/>
-            <Length title="Brake length" time={brakeTime} changeTime={changeTime} type='break'/>
-            <div>
-                <h3>Display time</h3>
-                <h1>{formatTime(displayTime)}</h1>
-                <button onClick={() => setIsPaused(false)}>Starting!</button>
-                <button onClick={stop}>Stop</button>
-            </div>
+return (
+    <div className="App">
+        <Length title="Pomodoro length" time={pomodoroTime} changeTime={changeTime} type='pomodoro'/>
+        <Length title="Brake length" time={brakeTime} changeTime={changeTime} type='break'/>
+        <div>
+            <h3>Display time</h3>
+            <h1>{formatTime(displayTime)}</h1>
+            <button onClick={() => setIsPaused(false)}>Starting!</button>
+            <button onClick={stop}>Stop</button>
         </div>
-    );
+    </div>
+);
 }
 
 export default App;
