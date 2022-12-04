04b4af47219edd6549cf65e14aec2adae0d51b2b
rusmarch <jakal5577>
2022-12-04 21:00:01 +0200

    8

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import {useState, useEffect} from "react";
 
 function App() {
 
-    const [displayTime, setDisplayTime] = useState(10);
+    const [pomodoroTime, setPomodoroTime] = useState(10);
+    const [brakeTime, setBrakeTime] = useState(5);
+    const [displayTime, setDisplayTime] = useState(pomodoroTime);
     const [isWorking, setIsWorking] = useState(false);
-    const [isPause, setIsPause] = useState(false);
-    const [textButton, setTextButton] = useState('Start')
-
+    const [isPaused, setIsPaused] = useState(false);
+    const [textButton, setTextButton] = useState('Start');
+    const [mode, setMode] = useState(true);
+    const [showMessage, setShowMessage] = useState('Lets to work!');
 
 // =============== Вынести в компонет Format Time ====================================
     const formatTime = (time) => {
@@ -21,38 +24,83 @@ function App() {
     }
 
 // =============================================================================
-
-    const changeTextButton = () => {
+    function start() {
         if (isWorking) {
-        setTextButton('Pause')
+            setIsPaused(true);
+        }
+
+        setIsWorking(!isWorking);
+        if (isWorking) {
+            setTextButton('Start');
+        } else {
+            setTextButton('Pause');
         }
     }
 
     function stop() {
         clearInterval(interval);
     }
+// ===============================================================================
+    function switchMode() {
+        setMode(!mode);
+        if (!mode) {
+            setDisplayTime(pomodoroTime);
+            setShowMessage('Time to work!');
+        } else {
+            setDisplayTime(brakeTime);
+            setShowMessage('Time to rest!');
+        }
+    }
+
+    function skipBrake() {
+        if (isPaused && mode) {
+            setTextButton('Skip brake');
+            clearInterval(interval);
+        }
+    }
 
     let interval;
 
-    use
+    useEffect(() => {
+        if (!isWorking) {
+            return;
+        } else {
+            interval = setInterval(() => {
+                setDisplayTime(displayTime - 1);
+            }, 1000)
+        }
+        if (isWorking && mode) {
+            setShowMessage('working time...')
+        } else if (isWorking && !mode) {
+            setShowMessage('resting time...')
+        }
+        if (displayTime === 0) {
+            stop();
+            setIsWorking(false);
+            setTextButton('Start');
+            switchMode();
+        }
+
+        return () => clearInterval(interval);
+    })
 
 // =============================================================================
 
+    console.log('mode: ' + mode);
 
     return (
         <div className="App">
             <div>
-                <h3>Display time</h3>
+                <h2>Display time</h2>
+                <h3>{showMessage}</h3>
                 <h1>{formatTime(displayTime)}</h1>
-                {!isPause
-                ? <button onClick={() => setIsWorking(true)}>{textButton}</button>
+                {!isPaused
+                    ? <button onClick={start}>{textButton}</button>
                     : <div>
-                        <button>Continue</button>
-                        <button>Stop</button>
+                        <button onClick={start}>Continue</button>
+                        <button onClick={stop}>Stop</button>
                     </div>
                 }
-
-
 
             </div>
         </div>
