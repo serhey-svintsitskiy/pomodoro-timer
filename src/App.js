5c40a4181e23d943daa0e8e8f85a920c820fa34b
rusmarch <jakal5577>
2022-12-04 11:27:59 +0200

    ;;;;;

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import React from "react";
 import {useState, useEffect} from "react";
-import Length from "./components/Length";
 
 function App() {
 
-    const [brakeTime, setBrakeTime] = useState(5);
-    const [pomodoroTime, setPomodoroTime] = useState(10);
-    const [displayTime, setDisplayTime] = useState(pomodoroTime);
-    const [mode, setMode] = useState(true);
-    const [isPaused, setIsPaused] = useState(true);
+    const [displayTime, setDisplayTime] = useState(10);
 
 // =============== Вынести в компонет Format Time ====================================
     const formatTime = (time) => {
@@ -21,62 +16,18 @@ function App() {
         );
     }
 
-// ===================--  --=======================================================
-
-
-    let interval;
-
-    function stop() {
-        clearInterval(interval);
-    }
-
-    useEffect(() => {
-        if (isPaused) {
-            return;
-        } else {
-            interval = setInterval(() => {
-                setDisplayTime(displayTime - 1);
-            },1000)
-        }
-
-        if (displayTime === 0) {
-            stop();
-            setIsPaused(true);
-            setMode(!mode);
-        }
-        return () => clearInterval(interval);
-    }, [displayTime, isPaused]);
-
-
-    console.log('mode: ' + mode);
-    console.log('isPaused: ' + isPaused);
-
+// =============================================================================
 
 
 
 // =============================================================================
 
 
-const changeTime = (amount, type) => {
-    if (type === 'pomodoro') {
-        setPomodoroTime((prev) => prev + amount);
-    } else {
-        setBrakeTime((prev) => prev + amount);
-    }
-}
-
-//console.log('mode: ' + mode);
-//console.log(displayTime);
-
 return (
     <div className="App">
-        <Length title="Pomodoro length" time={pomodoroTime} changeTime={changeTime} type='pomodoro'/>
-        <Length title="Brake length" time={brakeTime} changeTime={changeTime} type='break'/>
         <div>
             <h3>Display time</h3>
             <h1>{formatTime(displayTime)}</h1>
-            <button onClick={() => setIsPaused(false)}>Starting!</button>
-            <button onClick={stop}>Stop</button>
         </div>
     </div>
 );
